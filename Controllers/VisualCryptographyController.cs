82856a8c7d0f1e5c3e11554073913cfb14720971
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2018-09-26 07:03:52 +0200

    NE | controller + (secret  + watermarks view)
    głupotki

---
+++ Controllers/VisualCryptographyController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoWebService.Controllers
+{
+    public class VisualCryptographyController : Controller
+    {
+        #region Secret
+
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Secret(int result)
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region WaterMarks
+
+        public IActionResult WaterMarks()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult WaterMarks(int result)
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
