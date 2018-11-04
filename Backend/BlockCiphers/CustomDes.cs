20b93638d1734d6c8d0f96a4ad789c7c4be71300
grvx24 <kamil.sagalara@student.put.poznan.pl>
2018-11-05 00:13:12 +0100

    AesVizAlmostReady

--- Backend/BlockCiphers/CustomDes.cs
+++ Backend/BlockCiphers/CustomDes.cs
@@ -1,35 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace CryptoWebService.Backend.BlockCiphers
 {
     public class CustomDes : BlockCiphers
     {
-        public override byte[] GenerateKey(int length)
+    private DESCryptoServiceProvider des;
+
+    public CustomDes(bool padding = true)
+    {
+        this.des = new DESCryptoServiceProvider();
+        this.Padding = padding;
+    }
+
+    public override byte[] GenerateKey(int length)
+    {
+        des.KeySize = length;
+        des.GenerateKey();
+        return des.Key;
+    }
+
+    public override byte[] GenerateIV()
+    {
+        des.GenerateIV();
+        return des.IV;
+    }
+
+    public override byte[] Encrypt(byte[] message, byte[] key, byte[] IV)
+    {
+        des.KeySize = key.Length * 8;
+        des.Key = key;
+        SetCipherMode();
+
+
+        if (des.Mode != System.Security.Cryptography.CipherMode.ECB)
+            des.IV = IV;
+
+        var encryptor = des.CreateEncryptor(des.Key, des.IV);
+        byte[] encrypted;
+
+        using (MemoryStream ms = new MemoryStream())
         {
-            throw new NotImplementedException();
+            using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+            {
+                cs.Write(message);
+            }
+
+            encrypted = ms.ToArray();
         }
 
-        public override byte[] GenerateIV()
-        {
-            throw new NotImplementedException();
-        }
+        des.Dispose();
 
-        public override byte[] Encrypt(byte[] message, byte[] key, byte[] IV)
-        {
-            throw new NotImplementedException();
-        }
+        return encrypted;
+    }
 
-        public override byte[] Decrypt(byte[] message, byte[] key, byte[] IV)
-        {
-            throw new NotImplementedException();
-        }
+    public override byte[] Decrypt(byte[] message, byte[] key, byte[] IV)
+    {
+        byte[] decrypted = new byte[message.Length];
+        int decryptedLength = 0;
+        SetCipherMode();
 
-        protected override void SetCipherMode()
+        if (!this.Padding)
         {
-            throw new NotImplementedException();
+            des.Padding = PaddingMode.None;
+        }
+        des.KeySize = key.Length * 8;
+        des.Key = key;
+
+        if (des.Mode != System.Security.Cryptography.CipherMode.ECB)
+            des.IV = IV;
+
+        var decryptor = des.CreateDecryptor();
+
+        using (MemoryStream ms = new MemoryStream(message))
+        {
+            using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+            {
+                decryptedLength = cs.Read(decrypted);
+            }
+        }
+        byte[] result = new byte[decryptedLength];
+        Array.Copy(decrypted, result, decryptedLength);
+        des.Dispose();
+
+        return result;
+    }
+
+    protected override void SetCipherMode()
+    {
+        switch (CipherMode)
+        {
+            case BlockCipherMode.CBC:
+                des.Mode = System.Security.Cryptography.CipherMode.CBC;
+                break;
+            case BlockCipherMode.ECB:
+                des.Mode = System.Security.Cryptography.CipherMode.ECB;
+                break;
         }
     }
+}
 }
