1a6c79c1efa88b010f752bcc8d9cd01a8d7bc611
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2018-11-04 21:56:21 +0100

    change on view + json helper

---
+++ Helpers/JSONHelper.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace CryptoWebService.Helpers
+{
+    public static class JSONHelper
+    {
+        public static Object TransformListToJsonArray<T>(List<T> list)
+        {
+            JArray jArray = new JArray();
+            for (int i = 0; i < list.Count; i++)
+            {
+                jArray.Add(new JObject(new JProperty("value", list[i])));
+            }
+            return jArray;
+        }
+
+        public static Object TransformArrayToJsonArray<T>(T[] list)
+        {
+            JArray jArray = new JArray();
+            for (int i = 0; i < list.Length; i++)
+            {
+                jArray.Add(new JObject(new JProperty("value", list[i])));
+            }
+            return jArray;
+        }
+    }
+}
