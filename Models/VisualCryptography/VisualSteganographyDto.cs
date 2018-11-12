d07806f501d680d50a248cfa9d9028ad336e6d23
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2018-11-12 21:55:56 +0100

    visual steganography dto add and change on action page

---
+++ Models/VisualCryptography/VisualSteganographyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoWebService.Models.VisualCryptography
+{
+    public class VisualSteganographyDto
+    {
+        public int MethodId { get; set; }
+        public int AmountOfSecrets { get; set; }
+        public string Image { get; set; }
+    }
+}
