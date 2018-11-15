1fa10061937b764e937e01593d0dce4bcd56f965
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2018-11-15 19:10:34 +0100

    dto deleted

--- Models/VisualCryptography/VisualSteganographyDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CryptoWebService.Models.VisualCryptography
-{
-    public class VisualSteganographyDto
-    {
-        public int MethodId { get; set; }
-        public int AmountOfSecrets { get; set; }
-        public string Image { get; set; }
-    }
-}
