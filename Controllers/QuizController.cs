4410bc19dc5672e9f2e1a277f7c69297318a6dd3
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2018-12-25 19:20:29 +0100

    quiz controller

---
+++ Controllers/QuizController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Drawing;
+using System.IO;
+using CryptoWebService.Backend.VisualCryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+using CryptoWebService.Models.VisualCryptography;
+using CryptoWebService.Models;
+using CryptoWebService.Helpers;
+using CryptoWebService.Data;
+
+namespace CryptoWebService.Controllers
+{
+
+    public class QuizController : Controller
+    {
+        #region Secrets
+        [Route("quiz/{category?}/{id?}")]
+        public IActionResult quiz(string category, int? id)
+        {
+            if (String.IsNullOrEmpty(category))
+            {
+                // widok wszystkich dostępnych kategorii
+                //pobierz liste kategori
+                // var Categories = new List<Category>
+
+                return View("QuizzesCategory");
+            }
+            else
+            {
+                if(id == null || id == 0)
+                {
+                    //widok listy quizow z danej kategorii
+                    return View("QuizzesList");
+                }
+                else
+                {
+                    //widok quizu z danej kategori o danym id
+                    return View("Quiz");
+                }
+            }
+        }
+        #endregion
+
+    }
+}
