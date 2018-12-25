e96a1c81a5eb72d0cc354eb389a642df87f5828d
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2018-12-26 00:27:43 +0100

    add model classes

---
+++ Data/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoWebService.Data
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Quiz = new HashSet<Quiz>();
+        }
+
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+
+        public ICollection<Quiz> Quiz { get; set; }
+    }
+}
---
+++ Data/Quiz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoWebService.Data
+{
+    public partial class Quiz
+    {
+        public Quiz()
+        {
+            Question = new HashSet<Question>();
+        }
+
+        public int Id { get; set; }
+        public string QuizName { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+        public ICollection<Question> Question { get; set; }
+    }
+}
