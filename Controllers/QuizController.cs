f2c95bb3ae40c03ef52cc7caea2344c81491a83e
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2018-12-27 20:02:05 +0100

    quiz controller

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -5,6 +5,7 @@ using CryptoWebService.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using CryptoWebService.Models.Quiz;
 
 namespace CryptoWebService.Controllers
 {
@@ -29,24 +30,60 @@ namespace CryptoWebService.Controllers
                 // var Categories = new List<Category>
                 //var qs = new QuizService(new ApplicationDbContext(this.con));
 
-                var cat = _context.Category.ToList();
 
-                return View("QuizzesCategory", cat);
+                var Categories = _context.Category.ToList();
+
+                return View("QuizzesCategory", Categories);
             }
             else
             {
                 if(id == null || id == 0)
                 {
-                    //widok listy quizow z danej kategorii
-                    return View("QuizzesList");
+                    var Quizzes = _context.Quiz.Where(quiz => quiz.Category.CategoryName == category).ToList();
+                    return View("QuizzesList", Quizzes);
                 }
                 else
-                {
-                    //widok quizu z danej kategori o danym id
-                    return View("Quiz");
+                {  
+                    List<QuestionViewModel> questionViewModels = new List<QuestionViewModel>();
+                    List<AnswerViewModel> answerViewModels; 
+
+                    var quiz = _context.Quiz.Where(q => q.Id == id).FirstOrDefault();
+                    var Questions = _context.Question.Where(question => question.QuizId == quiz.Id).ToList();
+
+                    for (int i = 0; i < Questions.Count(); i++)
+                    {
+                        List<Answer> answers = _context.Answer.Where(a => a.QuestionId == Questions[i].Id).ToList();
+                        answerViewModels = new List<AnswerViewModel>();
+                        for (int j = 0; j<answers.Count(); j++)
+                        {
+                            answerViewModels.Add(new AnswerViewModel()
+                            {
+                                AnswerContent = answers[j].Content
+                            });
+                        }
+
+                        questionViewModels.Add(new QuestionViewModel()
+                        {
+                            QuestionContent = Questions[i].Content,
+                            Answers = answerViewModels,
+                            Number = i+1,
+                            AmountOfQuestionsInQuiz = Questions.Count()
+                        });
+                    }
+
+                    QuizViewModel quizViewModel = new QuizViewModel();
+                    quizViewModel.QuizName = quiz.QuizName;
+                    quizViewModel.Questions = questionViewModels;
+
+                    return View("Quiz", quizViewModel);
                 }
             }
         }
 
+
+        public List<String> GetCategories()
+        {
+            return _context.Category.Select(c => c.CategoryName).ToList();
+        }
     }
 }
