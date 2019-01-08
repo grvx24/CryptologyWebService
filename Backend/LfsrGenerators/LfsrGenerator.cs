085073824ed58ac0159378cc3fda4b2943e87876
grvx24 <kamil.sagalara@student.put.poznan.pl>
2019-01-09 00:10:49 +0100

    Lfsr last bit fix

--- Backend/LfsrGenerators/LfsrGenerator.cs
+++ Backend/LfsrGenerators/LfsrGenerator.cs
@@ -30,6 +30,7 @@ namespace CryptoWebService.Backend.LfsrGenerators
             {
                 Registers[registerIndex] = register;
             }
+            Registers[registerIndex].UpdateOutputBit();
         }
 
         protected virtual bool GenerateOneBit()
