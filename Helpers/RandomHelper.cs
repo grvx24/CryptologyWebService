96b7d9283260f965cf7559f2fd3682750ed44988
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2019-01-09 18:13:58 +0100

    random Helper

---
+++ Helpers/RandomHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoWebService.Helpers
+{
+    public static class RandomHelper
+    {
+        private static List<E> ShuffleList<E>(List<E> listToShuffle)
+        {
+            int randomIndex = 0;
+            Random r = new Random();
+            List<E> randomList = new List<E>();
+
+            while (listToShuffle.Count > 0)
+            {
+                randomIndex = r.Next(0, listToShuffle.Count);
+                randomList.Add(listToShuffle[randomIndex]);
+                listToShuffle.RemoveAt(randomIndex);
+            }
+            return randomList;
+        }
+    }
+}
