c1656690bf809cee98d23b0440d6b2c17ea2f7b3
Hubert Springer <Hubert.Springer.Kruszwica@gmail.com>
2019-01-15 00:15:57 +0100

    Update QuestionModel.cs

--- Models/Quiz/Model/QuestionModel.cs
+++ Models/Quiz/Model/QuestionModel.cs
@@ -5,6 +5,7 @@ namespace CryptoWebService.Models.Quiz.Model
     public class QuestionModel
     {
         public int QuizId { get; set; }
+        public int QuestionId { get; set; }
         public string QuestionContent { get; set; }
         public List<AnswerModel> Answers {get;set;}
     }
